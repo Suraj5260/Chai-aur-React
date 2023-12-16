03ad1a6d1c5ad0424a238ee5287120eb1ec50a37
Suraj5260 <suraj.telrandhe16@gmail.com>
2023-12-16 19:02:03 +0530

    jsx

--- 01vite/src/main.jsx
+++ 01vite/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Youtube from './Youtube.jsx'
 
+const ReactElement = React.createElement(
+  'a',
+  { Style: 'color: Red', href: 'https://www.google.com', target: '_blank' },
+  "click Me to gooo Google!!"
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <Youtube />
-  </React.StrictMode>,
+  ReactElement
 )
