a827efe8371ae1345de8164669f2f884895fb5f0
Suraj5260 <suraj.telrandhe16@gmail.com>
2024-01-16 19:35:06 +0530

    0

---
+++ playGround/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import './App.css'
+
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+//   const adder = () => {
+//     setCount(prevCount => prevCount + 1);
+//     setCount(prevCount => prevCount + 1);
+//     setCount(prevCount => prevCount + 1);
+//     setCount(prevCount => prevCount + 1);
+//     setCount(prevCount => prevCount + 1);
+//   }
+//   const subber = () => {
+//     setCount(count - 1);
+//   }
+
+
+//   return (
+//     <>
+//       <h1>Count is {count}</h1>
+//       <button onClick={adder}> ++ </button>
+//       <br />
+//       <button onClick={subber}> -- </button>
+//     </>
+//   )
+// }
+
+// BG changer
+
+function App() {
+
+  const [color, setColor] = useState("green")
+
+  return (
+    <>
+      <div className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }} >
+
+
+      </div>
+    </>
+
+  )
+}
+
+export default App
