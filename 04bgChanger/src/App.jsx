5f4fd5f3d9a2050d3660dc1b03cb80155fb4806a
Suraj5260 <suraj.telrandhe16@gmail.com>
2024-01-16 21:02:31 +0530

    0

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="w-full flex justify-center h-screen duration-200"
       style={{ backgroundColor: color }}
     >
-      <div className="  bg-white size-max justify-center outline-none px-4  py-1  rounded-full  shadow-lg">Color is {color}</div>
+      <div className="  bg-white size-max justify-center outline-none px-4  py-1 my-10  rounded-full  shadow-lg">Color is {color}</div>
       <div className=" fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 shadow-lg py-2 rounded-3xl ">
           <button
