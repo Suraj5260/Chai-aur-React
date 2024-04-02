f374f73e71a9ada2770115d237415d1fa10660ac
Suraj5260 <suraj.telrandhe16@gmail.com>
2024-04-02 13:42:40 +0530

    added lots of components and also added authLayout

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,11 +3,25 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button"
+import Input from "./Input"
+import Login from "./Login";
+import PostCard from "./PostCard"
+import Select from "./Select"
+import AuthLayout from "./AuthLayout"
+import Signup from "./Signup"
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
+    Login,
+    PostCard,
+    Select,
+    AuthLayout,
+    Signup,
 }
